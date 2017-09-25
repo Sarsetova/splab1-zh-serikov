c0c6b9c70526ad23c8f095cfb9d7dd269dfd3a88
zh-serikov <zhasulanserikov96@gmail.com>
2017-09-25 20:40:40 +0600

    Create exo.c

---
+++ exo.c
@@ -0,0 +1,38 @@
+/*
+Fullname: Zhasulan Serikov
+Group: 3EN04D
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+	
+	int c;
+	if ((c = getopt (argc, argv, "n:")) != -1) {
+		switch(c) {
+			case 'n':
+				for (int i = 2; i < argc ; i++)
+				{
+					printf("%s ", argv[i]);
+				}
+				printf("%s", argv[argc-1]);	
+				break;
+		}
+	}
+	else {
+		for (int i = 1; i < argc ; i++)
+		{
+			printf("%s ", argv[i]);
+		}
+		printf("\n");	
+	}
+
+	
+	return 0;
+}
